88ff45ceac77b0d10b17a403fa068322afa9ef25
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-03-15 11:09:07 +0500

    feat: solve reverse-int problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n === undefined) {
+        return "undefined";
+    } else if (n < 10 && n >= 0) {
+        return n;
+    }
+    let stringNumber = n.toString();
+    let answer = "";
+    for (let i = stringNumber.length - 1; i >= 0; i--) {
+        if (
+            (stringNumber[i] === 0 || stringNumber[i] === "-") &&
+            i === stringNumber.length - 1
+        ) {
+            continue;
+        }
+        answer += stringNumber[i];
+    }
+    return parseInt(answer);
+};
